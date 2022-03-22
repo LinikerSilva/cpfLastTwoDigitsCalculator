3eb418cd97d7c76320e6cfe4a1771ce6c3b0e15b
liniker.gsilva <liniker.gsilva@FRA0560571W10-1>
2022-03-22 17:12:05 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace cpfValidator
+{
+    partial class cpfValidator
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cpfTextBox = new System.Windows.Forms.TextBox();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cpfTextBox
+            // 
+            this.cpfTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cpfTextBox.Location = new System.Drawing.Point(156, 67);
+            this.cpfTextBox.Name = "cpfTextBox";
+            this.cpfTextBox.Size = new System.Drawing.Size(282, 35);
+            this.cpfTextBox.TabIndex = 0;
+            this.cpfTextBox.TextChanged += new System.EventHandler(this.cpfTextBox_TextChanged);
+            this.cpfTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cpfTextBox_KeyPress);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Yu Gothic UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(89, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(421, 40);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.Text = "Digite o CPF (Inclua os Pontos) :";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.titleLabel.Click += new System.EventHandler(this.titleLabel_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(193, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 60);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calcular os Digitos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Enabled = false;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(612, 93);
+            this.label1.TabIndex = 3;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cpfValidator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(614, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.cpfTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "cpfValidator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Validador de CPF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox cpfTextBox;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cpfValidator
+{
+    public partial class cpfValidator : Form
+    {
+        public cpfValidator()
+        {
+            InitializeComponent();
+        }
+
+        private void titleLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cpfTextBox_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            calcularDigitos();
+        }
+
+        private void cpfTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            calcularDigitos();
+        }
+
+        private void calcularDigitos()
+        {
+            int[] listaDosNumeros = new int[9];
+            int digit;
+            int[] resultadosDaMultiplicacao = new int[9];
+            int somaDosResultados = 0;
+            int primeiroDigitoCasoRestoDaDivisaoSejaMenorQueDois = 0;
+            int primeiroDigitoCasoContrario = 0;
+            int segundoDigitoCasoRestoDaDivisaoSejaMenorQueDois;
+            int segundoDigitoCasoContrario;
+            if (cpfTextBox.Text.Length == 9)
+            {
+                for (int i = 0; i < cpfTextBox.TextLength; i++)
+                {
+                    digit = Convert.ToInt32(cpfTextBox.Text[i].ToString());
+                    listaDosNumeros[i] = digit;
+                }
+
+                int cont = 10;
+                for (int i = 0; i < 9; i++)
+                {
+                    resultadosDaMultiplicacao[i] = listaDosNumeros[i] * cont;
+                    cont--;
+                }
+
+                for (int i = 0; i < 9; i++)
+                {
+                    somaDosResultados += resultadosDaMultiplicacao[i];
+                }
+
+                int[] listaDosNumerosComUmDigito = new int[10];
+                int restoDaDivisao = somaDosResultados % 11;
+                if (restoDaDivisao < 2)
+                {
+                    primeiroDigitoCasoRestoDaDivisaoSejaMenorQueDois = 0;
+                    listaDosNumerosComUmDigito[9] = primeiroDigitoCasoRestoDaDivisaoSejaMenorQueDois;
+                }
+                else
+                {
+                    primeiroDigitoCasoContrario = 11 - restoDaDivisao;
+                    listaDosNumerosComUmDigito[9] = primeiroDigitoCasoContrario;
+                }
+
+                digit = 0;
+                for (int i = 0; i < cpfTextBox.TextLength; i++)
+                {
+                    digit = Convert.ToInt32(cpfTextBox.Text[i].ToString());
+                    listaDosNumerosComUmDigito[i] = digit;
+                }
+
+                int[] resultadosDaMultiplicacaoUmDigito = new int[10];
+                cont = 11;
+                for (int i = 0; i < 10; i++)
+                {
+                    resultadosDaMultiplicacaoUmDigito[i] = listaDosNumerosComUmDigito[i] * cont;
+                    cont--;
+                }
+
+                somaDosResultados = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    somaDosResultados += resultadosDaMultiplicacaoUmDigito[i];
+                }
+
+                int[] listaDosNumerosComDoisDigitos = new int[11];
+                restoDaDivisao = 0;
+                restoDaDivisao = somaDosResultados % 11;
+                if (restoDaDivisao < 2)
+                {
+                    segundoDigitoCasoRestoDaDivisaoSejaMenorQueDois = 0;
+                    listaDosNumerosComDoisDigitos[10] = segundoDigitoCasoRestoDaDivisaoSejaMenorQueDois;
+                    listaDosNumerosComDoisDigitos[9] = primeiroDigitoCasoRestoDaDivisaoSejaMenorQueDois;
+
+                    int a = 0;
+                    foreach (int i in listaDosNumerosComUmDigito)
+                    {
+                        listaDosNumerosComDoisDigitos[a] = i;
+                        a++;
+                    }
+                    listaDosNumerosComDoisDigitos[10] = segundoDigitoCasoRestoDaDivisaoSejaMenorQueDois;
+
+                    string cpfCompleto = "";
+                    foreach (int i in listaDosNumerosComDoisDigitos)
+                    {
+                        cpfCompleto += i.ToString();
+                    }
+
+                    label1.Text = cpfCompleto;
+                    label1.Enabled = true;
+                }
+                else
+                {
+                    segundoDigitoCasoContrario = 11 - restoDaDivisao;
+                    listaDosNumerosComDoisDigitos[10] = segundoDigitoCasoContrario;
+                    listaDosNumerosComDoisDigitos[9] = primeiroDigitoCasoContrario;
+
+                    int a = 0;
+                    foreach (int i in listaDosNumerosComUmDigito)
+                    {
+                        listaDosNumerosComDoisDigitos[a] = i;
+                        a++;
+                    }
+                    listaDosNumerosComDoisDigitos[10] = segundoDigitoCasoContrario;
+
+                    string cpfCompleto = "";
+                    foreach (int i in listaDosNumerosComDoisDigitos)
+                    {
+                        cpfCompleto += i.ToString();
+                    }
+
+                    label1.Text = cpfCompleto;
+                    label1.Enabled = true;
+                }
+            }
+        }
+    }
+ }
